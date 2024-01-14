b2c621c1a1c4f0cd170155665eb8fe1e6e5d00b4
Adham-Magdy <adhammagdy349@gmail.com>
2024-01-14 16:58:10 +0200

    style: contact  page styling

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import styles from './page.module.css'
+import Image from 'next/image'
+import Button from '../components/Button/Button'
 
 const Contact = () => {
   return (
-    <div className={styles.contact}>
-      <h1>Contact</h1>
+    <div className={styles.container}>
+     <h1 className={styles.header}>Let&apos;s Keep In Touch</h1>
+     <div className={styles.content}>
+      <div className={styles.imgContainer}>
+        <Image
+        src={'/contact.png'}
+        alt=''
+        width={500}
+        height={500}
+        className={styles.image}
+        />
+      </div>
+      <form className={styles.form}>
+        <input type='text' placeholder='name' className={styles.input}/>
+        <input type='text' placeholder='email' className={styles.input}/>
+        <textarea
+        cols={30}
+        rows={10}
+        placeholder='message'
+        className={styles.textArea}
+        />
+        <Button url='#' text='Send'/>
+      </form>
+     </div>
     </div>
   )
 }
